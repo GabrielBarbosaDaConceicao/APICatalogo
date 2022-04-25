e8cacab9d3eec2596bfbf9adb4b02261b1b87153
GabrielBarbosaDaConceicao <gabriel.bc.dev@hotmail.com>
2022-04-25 16:02:21 -0300

    Add campo [JsonIgnore] para resolver problemas de serealizacao

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models
 {
@@ -28,6 +29,8 @@ namespace APICatalogo.Models
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
         public int CategoriaId { get; set; }
+
+        [JsonIgnore]
         public Categoria? Categoria { get; set; }
     }
 
